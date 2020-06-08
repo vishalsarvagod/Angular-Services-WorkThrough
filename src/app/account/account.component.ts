fc207ff0defb145d9bf51b4d319c6b13d8e5daad
Vishal Sarvagod <vishalsarvagod91@gmail.com>
2020-06-07 20:17:53 -0400

    Account-New-Account component added

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-account',
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+
+  onSetTo(status: string) {
+    this.statusChanged.emit({id: this.id, newStatus: status});
+    console.log('A server status changed, new status: ' + status);
+  }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-account',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent implements OnInit {
+  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountAdded.emit({
+      name: accountName,
+      status: accountStatus
+    });
+    console.log('A server status changed, new status: ' + accountStatus);
+  }
 }
